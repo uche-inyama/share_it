e581bc4ea4139519c8df1603f0747bab76875c2e
uche-inyama <depoulet@yahoo.com>
2020-04-22 15:22:01 +0100

    implement signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
