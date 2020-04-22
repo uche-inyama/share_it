3dc4e25aaca01abc95dc22b1ec631f58f618fd87
uche-inyama <depoulet@yahoo.com>
2020-04-22 12:17:11 +0100

    implement the new method in users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_013736) do
+ActiveRecord::Schema.define(version: 2020_04_22_104613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
