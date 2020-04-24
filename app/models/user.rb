b5fc882898e0bd480cb8cb9f95045bf233d4fe07
uche-inyama <depoulet@yahoo.com>
2020-04-24 16:33:17 +0100

    implement the association between the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     validates :fullname, presence: true
 
     has_many :followers, foreign_key: 'follower_id', class_name: 'Following'
-    has_many :followed, foreign_key: 'followed_id', class_name: 'Following'
+    has_many :followeds, foreign_key: 'followed_id', class_name: 'Following'
+    has_many :authored_opinions, foreign_key: 'author_id', class_name: 'Opinion'
 end
