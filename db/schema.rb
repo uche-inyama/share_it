0f4c7094523822ae2c08590ba98e372d6127ba5e
uche-inyama <depoulet@yahoo.com>
2020-05-12 21:17:35 +0100

    resolve migration error on followings table

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,13 @@ ActiveRecord::Schema.define(version: 2020_05_10_182025) do
   end
 
   create_table "followings", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_followings_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_followings_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
   end
 
   create_table "opinions", force: :cascade do |t|
