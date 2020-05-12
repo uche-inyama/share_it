1b17c399abcc962ac81278e211f93372bdc03a1c
uche-inyama <depoulet@yahoo.com>
2020-05-12 10:46:48 +0100

    delete add_attributes_to_following from migration file

--- db/migrate/20200424154406_add_attributes_to_following.rb
+++
@@ -1,6 +0,0 @@
-class AddAttributesToFollowing < ActiveRecord::Migration[6.0]
-  def change
-    add_column :followings, :follower_id, :integer
-    add_column :followings, :followed_id, :integer
-  end
-end
